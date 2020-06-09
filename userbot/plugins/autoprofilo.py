ff2bb29dd912bf2e62fe3ee871d9a881c52b1917
IOOIOIIIO <63565341+100101110@users.noreply.github.com>
2020-06-09 16:58:27 +0200

    Add files via upload

---
+++ userbot/plugins/autoprofilo.py
@@ -0,0 +1,175 @@
+"""Auto Profile Updation Commands
+.autoname for time along name 
+.autopic tilted image along with time
+.autobio  for time along with your bio
+.monkeybio set of funny bio's
+"""
+import asyncio
+import time
+import random, re
+import os
+import shutil
+
+from datetime import datetime
+from PIL import Image, ImageDraw, ImageFont
+from pySmartDL import SmartDL
+from telethon import events
+from telethon.tl import functions
+from telethon.errors import FloodWaitError
+from userbot import bot, ALIVE_NAME, DEFAULT_BIO, CMD_HELP
+from userbot.system import dev_cmd, command
+
+# ================= CONSTANT =================
+DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ"
+DEL_TIME_OUT = 30
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "100101110" 
+FONT_FILE_TO_USE = "/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf"
+# ============================================
+
+
+@command(pattern="^.autopic", outgoing=True)
+async def autopic(event):
+    await event.edit(f"Autopic avviato.") 
+    downloaded_file_name = "userbot/original_pic.png"
+    downloader = SmartDL(Var.DOWNLOAD_PFP_URL_CLOCK, downloaded_file_name, progress_bar=False)
+    downloader.start(blocking=False)
+    photo = "userbot/photo_pfp.png"
+    while not downloader.isFinished():
+        place_holder = None
+    counter = -30
+    while True:
+        shutil.copy(downloaded_file_name, photo)
+        im = Image.open(photo)
+        file_test = im.rotate(counter, expand=False).save(photo, "PNG")
+        current_time = datetime.now().strftime("⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡ \n  Time: %H:%M \n  Date: %d.%m.%y \n⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡⚡")
+        img = Image.open(photo)
+        drawn_text = ImageDraw.Draw(img)
+        fnt = ImageFont.truetype(FONT_FILE_TO_USE, 30)
+        drawn_text.text((350, 350), current_time, font=fnt, fill=(255, 255, 255))
+        img.save(photo)
+        file = await bot.upload_file(photo)  # pylint:disable=E0602
+        try:
+            await bot(functions.photos.UploadProfilePhotoRequest(  # pylint:disable=E0602
+                file
+            ))
+            os.remove(photo)
+            counter -= 30
+            await asyncio.sleep(30)
+        except:
+            return
+        
+@bot.on(dev_cmd(pattern="autoname"))  # pylint:disable=E0602
+async def _(event):
+    await event.edit(f"Auto Name avviato.") 
+    while True:
+        DM = time.strftime("%d-%m-%y")
+        HM = time.strftime("%H:%M")
+        name = f"{HM} {DEFAULTUSER} {DM}"
+        logger.info(name)
+        try:
+            await bot(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                first_name=name
+            ))
+        except FloodWaitError as ex:
+            logger.warning(str(e))
+            await asyncio.sleep(ex.seconds)
+    
+        if Var.PRIVATE_GROUP_ID:
+            await bot.send_message(
+                Var.PRIVATE_GROUP_ID, "#AUTONAME\n"
+                "Nome profilo aggiornato.")
+
+        await asyncio.sleep(DEL_TIME_OUT)
+    
+
+@bot.on(dev_cmd("autobio"))  # pylint:disable=E0602
+async def _(event):
+    await event.edit(f"Auto bio avviata.") 
+    while True:
+        DMY = time.strftime("%d.%m.%Y")
+        HM = time.strftime("%H:%M:%S")
+        bio = f"📅 {DMY} | {DEFAULTUSERBIO} | ⌚️ {HM}"
+        logger.info(bio)
+        try:
+            await bot(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                about=bio
+            ))
+        except FloodWaitError as ex:
+            logger.warning(str(e))
+            await asyncio.sleep(ex.seconds)
+
+        if Var.PRIVATE_GROUP_ID:
+            await bot.send_message(
+                Var.PRIVATE_GROUP_ID, "#AUTOBIO\n"
+                "Bio profilo aggiornata.")
+
+        await asyncio.sleep(DEL_TIME_OUT)
+     
+     
+BIO_STRINGS = [
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "🐵",
+     "🙈",
+     "🙉",
+     "🙊",
+     "🐵",
+     "🐵",
+     "🙈",
+     "🙉",
+     "🙊",
+     "🐵",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "ᗯᗩᏆᎢᏆᑎᏀ ᏞᏆᏦᗴ ᎢᏆᗰᗴ",
+     "🐵",
+     "🙈",
+     "🙉",
+     "🙊",
+     "🐵",
+     "🐵",
+     "🙈",
+     "🙉",
+     "🙊",
+     "🐵",
+
+]
+
+
+@bot.on(dev_cmd(pattern="monkeybio"))  # pylint:disable=E0602
+async def _(event):
+    await event.edit(f"monkey bio avviata.") 
+    while True:
+        bro = random.randint(0, len(BIO_STRINGS) - 1)    
+        #input_str = event.pattern_match.group(1)
+        Bio = BIO_STRINGS[bro]
+        logger.info(Bio)
+        try:
+            await bot(functions.account.UpdateProfileRequest(  # pylint:disable=E0602
+                about=Bio
+            ))
+        except FloodWaitError as ex:
+            logger.warning(str(e))
+            await asyncio.sleep(ex.seconds)
+
+        if Var.PRIVATE_GROUP_ID:
+            await bot.send_message(
+                Var.PRIVATE_GROUP_ID, "#MONKEYBIO\n"
+                "MonkeyBio attivata.")
+
+        await asyncio.sleep(DEL_TIME_OUT)
